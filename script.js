6ca2c8db99ef217c907f57b216237b3bbb2d828d
Rachel Fingerhut <fingerhutrachel@gmail.com>
2025-11-17 19:53:49 -0800

    feat(add new book): Form submits book information, stores book in an array, and displays newly added book as a book card.

--- script.js
+++ script.js
@@ -14,18 +14,30 @@ function addBookToLibrary(title, author){
 
     //store new book object into the array
     myLibrary.push(newBook);
+
+    //update display after book has been added
+    displayBook(newBook);
 }
 
-// FOR TESTING //
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien');
-addBookToLibrary('Pride and Prejudice', 'Jane Austen');
+const bookCardSection = document.getElementById('book-card-section');
 
-const body = document.getElementById('book-card-section');
+function displayBook(book){
+    const bookCard = document.createElement('div');
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
 
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookCard.id = book.id;
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCardSection.appendChild(bookCard);
 
-
+}
 
 function displayLibrary(){
+    bookCardSection.innerHTML = '';
+
     myLibrary.forEach((el) => {
         const bookCard = document.createElement('div');
         const bookTitle = document.createElement('p');
@@ -36,8 +48,26 @@ function displayLibrary(){
         bookCard.id = el.id;
         bookCard.appendChild(bookTitle);
         bookCard.appendChild(bookAuthor);
-        body.appendChild(bookCard);
+        bookCardSection.appendChild(bookCard);
     });
 }
 
+const addNewBookButton = document.getElementById('add-new-book-btn');
+const dialog = document.querySelector("dialog");
+const form = document.querySelector('form');
+
+
+addNewBookButton.addEventListener("click", () => dialog.show());
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const title = document.getElementById('book-title').value;
+    const author = document.getElementById('book-author').value;
+
+    addBookToLibrary(title, author);
+    form.reset();
+})
+
+// On browser load
 displayLibrary();
